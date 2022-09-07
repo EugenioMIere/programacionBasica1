edafb2aabb268e07321b99068d829df91d2ab2d0
EugenioMIere <emierearrua@alumno.unlam.edu.ar>
2022-09-07 20:57:17 -0300

    se crea clase circulo con metodos calcular area y perimetro

--- trabajosPracticos/src/ar/edu/unlam/pb1/trabajoPractico01/Circulo.java
+++ trabajosPracticos/src/ar/edu/unlam/pb1/trabajoPractico01/Circulo.java
@@ -1,10 +1,46 @@
 package ar.edu.unlam.pb1.trabajoPractico01;
 
 public class Circulo {
+	final static double PI=3.14;
 
 	public static void main(String[] args) {
-	System.out.println("Hola mundo");
-
+		
+		double radio =0.0, perimetro= 0.0,area= 0.0;
+		//Calcular el area de un circulo con radio 2
+		
+		radio= 2.0;
+		area= calcularElArea(radio);
+		
+		//Calcular el perimetro de un circulo con radio 2
+		
+		perimetro = calcularElPerimetro(radio);
+		
+		//Mostrar los resultados
+		
+		System.out.println("Area: "+area);
+		System.out.println("Perimetro: "+ perimetro);
+		
+		//Calcular el area de un circulo con radio 10
+		
+		radio = 10.0;
+		area = calcularElArea(radio);
+		//Mostrar los resultados
+		
+		System.out.println("Area: "+area);
+		
 	}
+	
+	public static double calcularElArea(double radio){
+		double area = 0.0;
+		area = radio*PI;
+		return area;
+		
+	}
+	public static double calcularElPerimetro(double radio){
+		double perimetro = 0.0;
+		perimetro = (2*PI)*radio;
+		return perimetro;
+	}
+	
 
 }
